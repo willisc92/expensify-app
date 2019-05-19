a09cb6a2699dca83e687c95760f9c3ad09684511
Willis Cheung <willis.cheung88@gmail.com>
2019-05-19 11:24:32 -0600

    Setup production build and server

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@
 const path = require('path');   // imports path
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-
 module.exports = (env) => {
     const isProduction = env === 'production';
     const CSSExtract = new ExtractTextPlugin('styles.css');
