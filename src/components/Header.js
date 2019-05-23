c721cce8b59d22bab338c06985df650dcdea374d
Willis Cheung <willis.cheung88@gmail.com>
2019-05-22 19:36:43 -0600

    Added styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,19 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { startLogout } from "../actions/auth";
 
 export const Header = ({ startLogout }) => (
-    <header>
-        <h1>Expensify</h1>
-        <div>
-            <NavLink to="/dashboard" activeClassName="is-active">
-                Dashboard
-            </NavLink>
-        </div>
-        <div>
-            <NavLink to="/create" activeClassName="is-active" exact={true}>
-                Create Expense
-            </NavLink>
-        </div>
-        <div>
-            <button onClick={startLogout}>Logout</button>
+    <header className="header">
+        <div className="content-container">
+            <div className="header__content">
+                <Link className="header__title" to="/dashboard">
+                    <h1>Expensify</h1>
+                </Link>
+                <button className="button button--link" onClick={startLogout}>
+                    Logout
+                </button>
+            </div>
         </div>
     </header>
 );
